97769e717a6a356875f775a4944a2673ffcd3fe7
Tuna Tuncer <66808459+kuantuna@users.noreply.github.com>
2020-11-14 16:06:12 +0300

    Create tuna_tuncer_hw1_DML.sql

---
+++ FoodHorse/tuna_tuncer_hw1_DML.sql
@@ -0,0 +1,23 @@
+INSERT INTO FoodHorse.Customer(fname, lname, caddress, pnum)
+VALUES 
+    ('Seçkin', 'Ağırbaş', 'Alibeyköy', '05001112233'),
+    ('Mahmut', 'Acar', 'Kartal', '05002221133'),
+    ('Merve', 'Özkan', 'Beşiktaş', '05003332211'),
+    ('Aysel', 'Tekin', 'Kadıköy', '05002223311'),
+    ('Orhan', 'Yavuz', 'Ataşehir', '05003331122');
+
+INSERT INTO FoodHorse.Branch(bname, baddress)
+VALUES
+    ('Alibeyköy Center', 'Alibeyköy'),
+    ('Kadıköy Center', 'Kadıköy'),
+    ('Kadıköy Pier', 'Kadıköy'),
+    ('Beşiktaş Square', 'Beşiktaş'),
+    ('Taksim Square', 'Taksim');
+
+INSERT INTO Foodhorse.Product(pname, pdescription, price)
+VALUES 
+    ('FoodHorse Olive Oil', 'FoodHorse brand olive oil 1L', 11.50),
+    ('FoodHorse Rice', 'FoodHorse brand rice 2.5kg', 13.75),
+    ('FoodHorse Milk', 'FoodHorse brand whole milk 1L', 3.75),
+    ('FoodHorse Kosher Dill Pickles', 'FoodHorse brand pickles 680g', 4.45),
+    ('FoodHorse Strawberry Jam', 'FoodHorse Strawberry Jam 380g', 6.45);
