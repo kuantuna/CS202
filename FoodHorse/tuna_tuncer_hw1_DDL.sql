e42c18afd75fd6031c0335045c437462a71b0640
Tuna Tuncer <66808459+kuantuna@users.noreply.github.com>
2020-11-14 16:05:33 +0300

    Update and rename CreateAndInsert.sql to tuna_tuncer_hw1_DDL.sql

---
+++ FoodHorse/tuna_tuncer_hw1_DDL.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS FoodHorse;
+
+CREATE TABLE IF NOT EXISTS FoodHorse.Customer(
+    cid int AUTO_INCREMENT PRIMARY KEY ,
+    fname varchar(30) NOT NULL,
+    lname varchar(30) NOT NULL,
+    caddress varchar(30) NOT NULL,
+    pnum varchar(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS FoodHorse.Branch(
+    bid int AUTO_INCREMENT PRIMARY KEY,
+    bname varchar(50) NOT NULL,
+    baddress varchar(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS FoodHorse.Product(
+    pid int AUTO_INCREMENT PRIMARY KEY,
+    pname varchar(50) NOT NULL,
+    pdescription varchar(100) NOT NULL,
+    price float NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS FoodHorse.Order(
+    oid int AUTO_INCREMENT PRIMARY KEY,
+    odate varchar(10) NOT NULL,
+    cid int,
+    pid int,
+    bid int,
+    FOREIGN KEY (cid) REFERENCES FoodHorse.Customer(cid),
+    FOREIGN KEY (pid) REFERENCES FoodHorse.Product(pid),
+    FOREIGN KEY (bid) REFERENCES FoodHorse.Branch(bid)
+);
