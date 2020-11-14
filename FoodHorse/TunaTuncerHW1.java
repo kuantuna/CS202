377cc167bb47116803c1cbf4809623ce905bdb7b
Tuna Tuncer <66808459+kuantuna@users.noreply.github.com>
2020-11-15 02:36:56 +0300

    Create TunaTuncerHW1.java

---
+++ FoodHorse/TunaTuncerHW1.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TunaTuncerHW1
+{
+    private static final String URL = "jdbc:mysql://localhost:3306/FoodHorse?useSSL=false";
+    private static final String username = "123";
+    private static final String password = "123";
+    private static Connection connection = null;
+
+    public static void establishConnection()
+    {
+        try{
+            connection = DriverManager.getConnection(URL, username, password);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection()
+    {
+        try{
+            if(connection != null)
+                connection.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        establishConnection();
+        // Whole code will be written there
+        closeConnection();
+    }
+}
